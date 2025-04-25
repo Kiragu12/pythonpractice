9f586fd40f31229a85738e7c7cf5306af0c1183d
Kiragu12 <macharia.kiragu@strathmore.edu>
2025-04-25 22:13:49 +0300

    check if no is odd or even

---
+++ check if no is even or odd1.py
@@ -0,0 +1,10 @@
+# Function to check if a number is even or odd
+
+def checkevenodd(number):
+    if number % 2 == 0:
+        print(" even")
+    else:
+        print(" odd")
+
+# Example usage
+checkevenodd(7)  
