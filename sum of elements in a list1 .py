7c0508d0c9279d33355f313019c52344b92f105f
Kiragu12 <macharia.kiragu@strathmore.edu>
2025-04-25 22:16:49 +0300

    sum of elements in a list

---
+++ sum of elements in a list1 .py
@@ -0,0 +1,11 @@
+# Function to sum all elements in a list
+ages = [2, 3, 4, 5, 6, 7, 8, 9]
+def sum_list(numbers):
+    total = 0
+    for number in numbers:
+        total =total + number #
+    return total
+
+# Example usage
+
+print(sum_list(ages))  #
